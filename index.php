f5e203fd72047bd7e5f8c2119bee18dec14efcfd
PS229397 <PS229397@summacollege.nl>
2022-05-19 13:26:07 +0200

    moved everything to index

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <script src="./Javascript/script.js"></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
         <link rel="stylesheet" href="./Css/desktop.css">
         <link rel="stylesheet" href="./Css/mobile.css">
         <link rel="icon" type="" href="">
@@ -12,20 +13,49 @@
         <title>The Music Box</title>
     </head>
     <body>
-        <!-- grid layout -->
-        <div class="grid-container">
-            <!-- header/menu -->
-            <div class="item1">
-                <?php
-                    include 'main.php';
-                ?>
+        <!-- Menu -->
+        <header id="nav-wrapper">
+            <nav id="nav">
+            <div class="nav left">
+                <span class="gradient skew"><h1 class="logo un-skew"><a href="#home">The Music Box</a></h1></span>
+                <button id="menu" class="btn-nav"><span class="fas fa-bars"></span></button>
             </div>
-            <!-- site footer -->
-            <div class="item5"> 
-                <?php 
-                  include 'footer.php';
-                ?>
+            <div class="nav right">
+                <a href="#rap" class="nav-link"><span class="nav-link-span"><span class="u-nav">Rap</span></span></a>
+                <a href="#rock" class="nav-link active"><span class="nav-link-span"><span class="u-nav">Rock</span></span></a>
+                <a href="#dubstep" class="nav-link"><span class="nav-link-span"><span class="u-nav">Dubstep</span></span></a>
+                <a href="#dnb" class="nav-link"><span class="nav-link-span"><span class="u-nav">Drum 'n' Bass</span></span></a>
             </div>
-        </div>
+            </nav>
+        </header>
+        <main>
+            <section id="rap">
+                <div class="pagecontent">
+                    <br><br><br>
+                    hello world
+                </div>
+            </section>
+            <section id="rock">
+                <div class="pagecontent">
+                    <div class="innercontent">
+                        
+                    </div>
+                </div>
+            </section>
+            <section id="dubstep">
+                <div class="pagecontent">
+                    <div class="innercontent">
+                       
+                    </div>
+                </div>
+            </section>
+            <section id="dnb">
+                <div class="pagecontent">
+                    <div class="innercontent">
+
+                    </div>
+                </div>
+            </section>
+        </main>
     </body>
 </html>
