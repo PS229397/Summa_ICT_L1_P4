42de059c2d6c391e6a45e4793e0c96bef8c18eb8
PS229397 <PS229397@summacollege.nl>
2022-05-19 11:36:24 +0200

    Css work

---
+++ main.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <script src="./Javascript/script.js"></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+        <link rel="stylesheet" href="./Css/desktop.css">
+        <link rel="stylesheet" href="./Css/mobile.css">
+        <link rel="icon" type="" href="">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width= 1080px, initial-scale=1.0">
+        <meta name="description" content="">
+        <title>The Music Box</title>
+    </head>
+    <body>
+        <!-- Menu -->
+        <header id="nav-wrapper">
+            <nav id="nav">
+            <div class="nav left">
+                <span class="gradient skew"><h1 class="logo un-skew"><a href="#home">The Music Box</a></h1></span>
+                <button id="menu" class="btn-nav"><span class="fas fa-bars"></span></button>
+            </div>
+            <div class="nav right">
+                <a href="#rap" class="nav-link"><span class="nav-link-span"><span class="u-nav">Rap</span></span></a>
+                <a href="#rock" class="nav-link active"><span class="nav-link-span"><span class="u-nav">Rock</span></span></a>
+                <a href="#dubstep" class="nav-link"><span class="nav-link-span"><span class="u-nav">Dubstep</span></span></a>
+                <a href="#dnb" class="nav-link"><span class="nav-link-span"><span class="u-nav">Drum 'n' Bass</span></span></a>
+            </div>
+            </nav>
+        </header>
+        <main>
+            <section id="rap">
+                <div class="pagecontent">
+                    <div class="innercontent">
+                       
+                    </div>
+                </div>
+            </section>
+            <section id="rock">
+                <div class="pagecontent">
+                    <div class="innercontent">
+                        
+                    </div>
+                </div>
+            </section>
+            <section id="dubstep">
+                <div class="pagecontent">
+                    <div class="innercontent">
+                       > 
+                    </div>
+                </div>
+            </section>
+            <section id="dnb">
+                <div class="pagecontent">
+                    <div class="innercontent">
+                        
+                    </div>
+                </div>
+            </section>
+        </main>
+    </body>
+</html>
--- misc/template_temp.php
+++ misc/template_temp.php
@@ -7,21 +7,35 @@
         <link rel="icon" type="" href="">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width= 1080px, initial-scale=1.0">
+        <meta name="viewport" content="width= 414px, initial-scale=1.0">
         <meta name="description" content="">
-        <title>The Music Box</title>
+        <title>Jesse Keijzers</title>
     </head>
     <body>
         <!-- grid layout -->
         <div class="grid-container">
             <!-- header/menu -->
             <div class="item1">
-                <?php
-                    include 'main.php';
+                Menu 
+                <?php 
+                 include 'menu.php';
                 ?>
             </div>
+            <!-- pagina content -->
+            <div class="item2">
+                Main
+            </div>
+            <!-- left banner  -->
+            <div class="item3">
+                Right
+            </div>
+            <!-- right banner -->
+            <div class="item4">
+                Left
+            </div>
             <!-- site footer -->
-            <div class="item5"> 
+            <div class="item5">
+                Footer 
                 <?php 
                   include 'footer.php';
                 ?>
